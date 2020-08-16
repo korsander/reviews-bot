d6bbdd6ee49a4abdc752ddcc3752346773168ee5
korsander <korsander@gmail.com>
2020-08-16 13:09:57 +0300

    check interactive messages

--- bot/events/events_handler.go
+++ bot/events/events_handler.go
@@ -56,14 +56,73 @@ func StartEventsHandle(api *slack.Client) {
 			innerEvent := eventsAPIEvent.InnerEvent
 			switch ev := innerEvent.Data.(type) {
 			case *slackevents.AppMentionEvent:
-				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+				_, _, err = api.PostMessage(
+					ev.Channel,
+					slack.MsgOptionText("Would you like to build app?", false),
+					getSelectPlatformMsgOptions(),
+				)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					log.Printf("error send: %s\n", err)
+				}
 			}
 		}
 	})
+
+	http.HandleFunc("/slack/interactive-endpoint", func(w http.ResponseWriter, r *http.Request) {
+		buf := new(bytes.Buffer)
+		_, err := buf.ReadFrom(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error parse: %s\n", err)
+			return
+		}
+		body := buf.String()
+		log.Printf("incoming request %s\n", body)
+	})
+
 	log.Println("\n[INFO] Server listening")
 
 	err := http.ListenAndServeTLS(eventsAddr, fullChain, privKey, nil)
 	if err != nil {
 		log.Printf("error listen: %s", err)
 	}
+}
+
+func NewDivider() slack.Block {
+	return slack.DividerBlock{
+		Type: "divider",
+	}
+}
+
+func NewSection(text string) slack.Block {
+	return slack.SectionBlock{
+		Type: "section",
+		Text: &slack.TextBlockObject{
+			Type: "mrkdwn",
+			Text: text,
+		},
+	}
+}
+
+func getSelectPlatformMsgOptions() slack.MsgOption {
+	hello := NewSection("Welcome to Jenkins build bot")
+	divider := NewDivider()
+	prompt := NewSection("Which app would you like to build?")
+
+	actions := slack.NewActionBlock(
+		"test",
+		slack.NewButtonBlockElement(
+			"test1",
+			"android",
+			slack.NewTextBlockObject("plain_text", "Android", false, false),
+		),
+		slack.NewButtonBlockElement(
+			"test2",
+			"ios",
+			slack.NewTextBlockObject("plain_text", "iOS", false, false),
+		),
+	)
+
+	return slack.MsgOptionBlocks(hello, divider, prompt, actions)
 }
